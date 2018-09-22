1d953ca513a2e23b19f7b704af4d972083c82459
root <root@DOCKER-NODE-ONE.local>
2018-09-22 17:58:32 -0300

    ...

---
+++ tcc-project/backup/_index.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+print "Content-type: text/html \n"
+
+print """
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Container</title>
+	<style>
+		body{
+			margin-left: 5%;
+			margin-right: 5%;
+			font-size: large;
+			font-weight: bold;
+			text-align: center;
+		}
+	</style>
+
+</head>
+<body>
+	<h1> DOCKER - SERVICE </h1>
+</body>
+</html>
+"""
